9adfe2550cfb3fbd5ed1ab35886d4bf11bba901c
JaxisArty <jaxis0810@gmail.com>
2022-09-11 11:50:23 -0500

    urls hechas

---
+++ backend/blog/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import BlogListView, PostDetailView
+
+app_name = 'blog'
+
+urlpatterns = [
+    path('posts/', BlogListView.as_view()),
+    path('posts/<post_slug>/', PostDetailView.as_view())
+]
--- backend/core/urls.py
+++ backend/core/urls.py
@@ -5,7 +5,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls'))
+    path('blog/', include('blog.urls', namespace='blog')),
+    path('api-auth/', include('rest_framework.urls')),
 ]
 
 if settings.DEBUG:
